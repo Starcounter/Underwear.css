58ff2d4a343e6a8b568b83b80140ad7fac1e7dc6
Omar Alshaker <omar@omaralshaker.com>
2018-01-04 15:36:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const font = require('./src/font-import.css');
+const normalize = require('normalize.css');
+const css = require('milligram');
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const plugins = [
+  new ExtractTextPlugin("dist/underwear.css"),
+  new WebpackShellPlugin({
+    onBuildEnd: ["rm bundle.js || del bundle.js"]
+  })
+];
+
+const pluginsWithMinification = [
+  new ExtractTextPlugin("dist/underwear.min.css"),
+  new WebpackShellPlugin({
+    onBuildEnd: ["rm bundle.js || del bundle.js"]
+  }),
+  new OptimizeCssAssetsPlugin({
+    assetNameRegExp: /\.css$/g,
+    cssProcessor: require("cssnano"),
+    cssProcessorOptions: { discardComments: { removeAll: true } },
+    canPrint: true
+  })
+];
+
+const config = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: "file-loader",
+        options: {
+          name: "./dist/fonts/[name].[ext]"
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  }
+};
+
+const configUnminified = { ...config, plugins };
+const configMinified = { ...config, plugins: pluginsWithMinification };
+
+module.exports = [configUnminified, configMinified];
